dc88db6431401248cf985faf741e26a4ed3cf69d
programer665 <raj2002dixit@gmail.com>
2024-03-12 21:09:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+gsap.to("li",{
+    x:20,
+    y:20,
+    delay:2,
+    
+   
+})
+gsap.to("#nav-part2",{
+    x:20,
+    y:20,
+    delay:2,
+    
+   
+})
+gsap.to("#para",{
+    x:-10,
+    y:20,
+    delay:2,
+    
+   
+})
+gsap.to("#box1",{
+  
+    y:20,
+
+    rotate:(360),
+    speed:0.5,
+    delay:2,
+    
+   
+})
+gsap.to("#box2",{
+    x:-10,
+    y:20,
+    delay:2,
+   
+    
+    rotate:(-360)
+    
+   
+})
+gsap.to("#box3",{
+    x:40,
+    y:20,
+    delay:2,
+    rotate:(-360)
+    
+   
+})
